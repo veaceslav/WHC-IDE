264b27e1559f6487a8e37d8cb0b2db7f89a166d5
Andrei Preda <andrei@andrei-G74Sx.(none)>
2013-07-26 01:43:18 +0300

    Added missing copyright comments

--- src/monitor.cpp
+++ src/monitor.cpp
@@ -1,3 +1,26 @@
+/* ============================================================
+ *
+ * This file is a part of WHC IDE Project
+ * http://http://whcomputing.wikispaces.com/
+ *
+ * Date        : 23 JUL 2013
+ * Description : Implementation for the monitor class, responsible for logging.
+ *
+ * Copyright (C) 2013 by Preda Andrei <andreipreda07 at gmail dot com>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
 #include "monitor.h"
 
 #include <QStringList>
