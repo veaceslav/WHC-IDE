5d89da9e7489e740b992a44bbf187fd94e63b524
Andrei Preda <andreipreda07@gmail.com>
2014-04-05 11:35:33 +0300

    Fixed lines over 80 characters in src/ide.cpp
    Signed-off-by: Andrei Preda <andreipreda07@gmail.com>

--- src/ide.cpp
+++ src/ide.cpp
@@ -536,8 +536,10 @@ void Ide::startNewProject(QString whcFiles)
     diagramShow = true;
     diagramSubW->show();
 
-    connect(model, SIGNAL(updateDiagram(QString, QString)), diagram, SLOT(renameDiagItems(QString, QString)));
-    connect(model, SIGNAL(updateDiagram(QString,QString)), diagram, SLOT(renameTaskMenu(QString,QString)));
+    connect(model, SIGNAL(updateDiagram(QString, QString)),
+            diagram, SLOT(renameDiagItems(QString, QString)));
+    connect(model, SIGNAL(updateDiagram(QString,QString)),
+            diagram, SLOT(renameTaskMenu(QString,QString)));
 
     /**
      * after staring the project, we must enable this options
